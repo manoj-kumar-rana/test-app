e7b7e15c839e4765549c84651014f43edb8db987
Manoj Kumar Rana <manojsilicon08@gmail.com>
2020-09-01 17:58:56 +0530

    for war file making changes

--- src/main/java/com/manojrana/app/TestAppApplication.java
+++ src/main/java/com/manojrana/app/TestAppApplication.java
@@ -2,11 +2,16 @@ package com.manojrana.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
+//public class TestAppApplication {
+//}
+
 @SpringBootApplication
-public class TestAppApplication {
+public class TestAppApplication extends SpringBootServletInitializer{
 
 	@Bean
 	public RestTemplate getRestTemplate() {
